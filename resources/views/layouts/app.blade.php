40ca13886258d14044f4fea7b5332459727e2119
Muhozi Emery <muhozie@gmail.com>
2017-08-31 07:00:27 +0200

    Fix Redirection after registering & add events link in the menus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -166,6 +166,11 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li>
+                                <a href="{{ route('events') }}">
+                                    Events
+                                </a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->firstname . " " . Auth::user()->lastname}} <span class="caret"></span>
